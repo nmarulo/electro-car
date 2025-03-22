ca2085f98c8526229786a1e7e686bfc579627148
Nicolás Marulanda P <nmarulo@outlook.com>
2025-03-22 13:35:18 +0100

    Agregar características i18n - Configuración básica. - Nuevo componente "LanguageButton".

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,11 @@
 import {defineConfig} from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['es', 'en'],
+        routing: {
+            prefixDefaultLocale: true
+        }
+    }
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,11 @@
+export const languages = {
+  en: 'English',
+  es: 'Español'
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {},
+  es: {}
+} as const;
